ad3c0469cbbaabc6747dcec30eed48e6d5b644eb
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-12-17 12:53:02 -0600

    Update and rename Figure_S1b.R to Figure_S2b.R

--- Figures/Figure_S2b.R
+++ Figures/Figure_S2b.R
@@ -7,7 +7,7 @@ library(ggplot2)
 Bubble <- ggplot(fgseaRes_Tidy_1, aes(reorder(pathway, NES), NES, size=covered, color=padj))+ #
   geom_point(alpha=0.8)+coord_flip() +
   labs(x="Significant Hallmark Pathways", y="Normalized Enrichment Score",
-       title="Significant Hallmark pathways NES from GSEA between post and pre treatment in TUMOR") + 
+       title="Significant Hallmark pathways NES from GSEA between on and pre treatment in TUMOR") + 
   theme_minimal()+ scale_color_gradient(low="red", high="blue")+
   scale_size(range = c(4, 6), name="Coverage, [%]")+
   theme(axis.text = element_text(size=18),strip.text = element_text(size=18))+
