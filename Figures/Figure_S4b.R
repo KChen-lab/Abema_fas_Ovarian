ef4f24dfd2dfb8ad45ff64093e63ce0357caf188
NOLermi <92048889+NOLermi@users.noreply.github.com>
2025-12-17 15:10:48 -0600

    Figure_S4b.R

---
+++ Figures/Figure_S4b.R
@@ -0,0 +1,12 @@
+## Source data of this plot is in supplementary files of the paper.
+library(ggplot2)
+
+Bubble <- ggplot(fgseaRes_Tidy_1, aes(reorder(pathway, NES), NES, size=coverage, color=padj))+
+  geom_point(alpha=0.8)+coord_flip() +
+  labs(x="", y="Normalized Enrichment Score",
+       title="pathways NES from GSEA between on and pre treatment in Tumor with Long PFS") + 
+  theme_minimal()+ scale_color_gradient(low="red", high="blue")+
+  scale_size(range = c(4, 6), name="Coverage, [%]")+
+  theme(axis.text = element_text(size=18),strip.text = element_text(size=18))+
+  theme(legend.position  = "right", legend.text = element_text(size=18),title = element_text(size=18))
+Bubble
