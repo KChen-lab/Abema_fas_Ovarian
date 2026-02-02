3830633bd55b869ff1af53360b6a8646e30eba4a
NOLermi <92048889+NOLermi@users.noreply.github.com>
2026-02-02 10:40:33 -0600

    Update and rename Figure_5d.R to Figure_6d_1.R

--- Figures/Figure_6d_1.R
+++ Figures/Figure_6d_1.R
@@ -1,14 +1,15 @@
 ## Source data of this plot is in supplementary files of the paper.
 
+## DEGs of on-treatment vs pre-treatment samples in patients with long PFS (tumor compartment)
 library(ggplot2)
 library(ggrepel)
-Tumor_long_post_vs_pre_DEG <- read_csv("path/to/Figure_5d.csv")
+Tumor_long_post_vs_pre_DEG <- read_csv("path/to/Figure_6d.csv")
 colnames(Tumor_long_post_vs_pre_DEG)[1] <- "Gene"
 
-Gene_highlight <- c("UPK1B","WNK4","MIF","NT5E","H1-5","MYCN","THY1","IFITM1","IL32","KLK5","H3C2","TAGLN","CLDN1",
-                    "CLDN16","SPP1","UQCR10","TP53","KLK10","WNT7A","CCND1","CXCL12","LIF","CDK18","NDUFB6","HMGA1",
-                    "HMGB2","CDK1","IL4R","PCNA","PKM","GREB1","HLA-C","IFITM3","ISG20","MKI67","IGHG4","APOD","IGKC",
-                    "IGHG3","CXCL14","IGHG2","IL6","CCL4L2","MGP","NR4A1","FOS","DUSP1","JUNB","HSP90B1","EGR1","ATF3")
+Gene_highlight <- c("PER1","TOMM20","CLU","CXCL12", 'HLA-C', 'HLA-B','LRRC15','CD14', 'SDC1','IGHA1','CD14','HLA-A', 'FAP',"NT5E",
+                    "ACTA2","PDGFRB","THBS2",'C1QC',"POSTN","C1QB","SPP1","LUM",
+                    "CD81",'IL6ST',"ATP9A","EGR3","CCND2",'JUND',"NDUFB9","MYC","KRT19","ATP1B1","TUBB2B","JUNB","MGP",
+                    "ELF3","THSD4","FKBP5","ATF3","KRT18","EGR1","ESR1","KRT8","KLF4","DUSP1","NR4A1","FOS")
 
 Tumor_long_post_vs_pre_DEG$invert_P <- (-log10(Tumor_long_post_vs_pre_DEG$adj.P.Val)) 
 colnames(Tumor_long_post_vs_pre_DEG)[1] <- "Gene"
@@ -28,8 +29,6 @@ Tumor_long_post_vs_pre_DEG$Color <- factor(Tumor_long_post_vs_pre_DEG$Color, lev
 
 
 ggplot(Tumor_long_post_vs_pre_DEG, aes(x = logFC, y = invert_P, color = Color,label = Gene)) +
-  #geom_vline(xintercept = c(1, -1), lty = "dashed") +
-  #geom_hline(yintercept = -log10(0.05), lty = "dashed") +
   geom_point(size=0.3) +
   labs(x = "Pre-treatment <- log2(FC) -> On-treatment",
        y = "-log10(p.adj)",
