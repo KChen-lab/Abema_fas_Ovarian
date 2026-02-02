60eac70dd8ec129d9ef615ca9b70c662b59c5fe0
NOLermi <92048889+NOLermi@users.noreply.github.com>
2026-02-02 10:51:24 -0600

    Update and rename Figure_6f.R to Figure_7f.R

--- Figures/Figure_7f.R
+++ Figures/Figure_7f.R
@@ -1,5 +1,5 @@
 ## Source data of this plot is in supplementary files of the paper.
-
+## Heatmap showing expressions of key genes in tumor segments
 set.seed(2023)
 
 library(tidyverse)
@@ -8,8 +8,8 @@ library(ComplexHeatmap)
 library(circlize)
 library(RColorBrewer)
 
-mat.z = read.csv("path-to-folder/Figure_6_f.csv")
-meta_data = read.csv("path-to-folder/Figure_6_f_metadata.csv")
+mat.z = read.csv("path-to-folder/Figure_7f.csv")
+meta_data = read.csv("path-to-folder/Figure_7f_metadata.csv")
 
 col_fun = colorRamp2(c(-1,0,1), c("purple", "black", "yellow3"))
 
@@ -20,7 +20,8 @@ ann <- data.frame(
 
 # create the colour mapping
 colours <- list(
-  Patients = c("8"="#fbb463","13"="#8ed1c6"))
+  Patients = c("8"="#fbb463","13"="#8ed1c6","11"= "purple"),
+  PFS = c("Short" = "brown", "Long"= "blue"))
 
 
 # now create the ComplexHeatmap annotation object
@@ -37,18 +38,27 @@ colAnn <- HeatmapAnnotation(
   gap = unit(1, 'mm'),
   annotation_legend_param = list(
     Patients = list(
-      nrow = 4, # number of rows across which the legend will be arranged
+      nrow = 3, # number of rows across which the legend will be arranged
       title = 'Sample ID',
       title_position = 'topleft',
       
       legend_direction = 'horizontal',
-      title_gp = gpar(fontsize = 18, fontface = 'bold'),
-      labels_gp = gpar(fontsize = 18))))
+      title_gp = gpar(fontsize = 14, fontface = 'bold'),
+      labels_gp = gpar(fontsize = 14)),
+    PFS= list(
+      nrow = 2, # number of rows across which the legend will be arranged
+      title = 'PFS',
+      title_position = 'topleft',
+      
+      legend_direction = 'horizontal',
+      title_gp = gpar(fontsize = 14, fontface = 'bold'),
+      labels_gp = gpar(fontsize = 14))))
+    
     
     
 
 
-split <- factor(ann$Patients, levels=c("8","13"))
+split <- factor(ann$PFS, levels=c("Short","Long"))
 ht_list = Heatmap(t(mat.z),name="z score", col=col_fun,cluster_columns = T,cluster_rows = T,
                   heatmap_legend_param = list(
                     color_bar = 'continuous',
