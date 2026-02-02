94db14e79a9f75769f0422445f17874e7e751a81
NOLermi <92048889+NOLermi@users.noreply.github.com>
2026-02-02 11:28:52 -0600

    Update and rename Figure_S4a.R to Figure_S5a.R

--- Figures/Figure_S5a.R
+++ Figures/Figure_S5a.R
@@ -1,4 +1,5 @@
 ## Source data of this plot is in supplementary files of the paper.
+## GSEA of short vs. long PFS patient samples at pre-treatment timepoint (tumor compartment) using published metaprograms (Gavish et. al, Nature 2023)
 library(ggplot2)
 
 Bubble <- ggplot(fgseaRes_Tidy_1, aes(reorder(pathway, NES), NES, size=coverage, color=padj))+
